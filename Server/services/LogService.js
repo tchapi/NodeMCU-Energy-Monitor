ae968c121442730d7ceaca74f6f0572bdf7459ce
tchapi <regbasket@gmail.com>
2015-12-16 00:06:45 +0100

    Change date format

--- Server/services/LogService.js
+++ Server/services/LogService.js
@@ -9,7 +9,7 @@ var p = LogService.prototype
 p.append = function(data) {
 
   var d = new Date()
-  date = d.getFullYear() + "/" + ('0' + (d.getMonth()+1)).slice(-2) + "/" + ('0' + d.getDate()).slice(-2) + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+  date = d.getFullYear() + "/" + ('0' + (d.getMonth()+1)).slice(-2) + "/" + ('0' + d.getDate()).slice(-2) + " " + ('0' + d.getHours()).slice(-2) + ":" + ('0' + d.getMinutes()).slice(-2) + ":" + ('0' + d.getSeconds()).slice(-2)
 
   // Format log
   /*
